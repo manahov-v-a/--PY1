669252b2b96de65cd06fd44a186f4e43f77b87a0
manahov-v-a <115486800+manahov-v-a@users.noreply.github.com>
2022-10-11 19:37:54 +0300

    Update task6.py

--- Laba3/task6.py
+++ Laba3/task6.py
@@ -1,9 +1,8 @@
-src = not False and True or False and not True
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-result = True or False and False  # избавляемся от отрицаний
-result = True or False  # избавляемся от логического "И"
-result = True  # избавляемся от логического "ИЛИ"
+a = list_numbers[len(list_numbers)-1]
+list_numbers[len(list_numbers)-1] = list_numbers[list_numbers.index(max(list_numbers))]
+list_numbers[list_numbers.index(max(list_numbers))] = a
 
-result = True
+print(list_numbers)
 
-print(src == result)

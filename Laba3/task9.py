94a5b686f657b87387735874962d443981687025
manahov-v-a <115486800+manahov-v-a@users.noreply.github.com>
2022-10-11 19:38:28 +0300

    Update task9.py

--- Laba3/task9.py
+++ Laba3/task9.py
@@ -1,13 +1,11 @@
-BYTES_ONE_CHAR = 1  # размер одного символа в байтах
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
 
-# никаких магических чисел
-pages = 100
-lines = 50
-chars = 25
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
 
-total_chars = chars*lines*pages
-total_bytes = total_chars
+for i in range(10):
+    need_money += spend*(1+increase)**i - salary
 
-disk_size = 1.44*1024**2
-
-print(disk_size//total_bytes)
+print(round(need_money))

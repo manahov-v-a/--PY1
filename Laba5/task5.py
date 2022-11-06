50876a015834453c20fae7310203f3ee86bf8c9d
manahov-v-a <115486800+manahov-v-a@users.noreply.github.com>
2022-11-06 18:46:14 +0300

    Update task5.py

--- Laba5/task5.py
+++ Laba5/task5.py
@@ -2,8 +2,8 @@ import random
 import string
 
 
-def get_random_password() -> str:
-    return ''.join(random.sample(string.ascii_letters + string.digits, 8))
+def get_random_password(equal=8) -> str:
+    return ''.join(random.sample(string.ascii_letters + string.digits, equal))
 
 
 print(get_random_password())
